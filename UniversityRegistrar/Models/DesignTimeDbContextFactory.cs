1dd99acc225ef948938b657ea497a70880508c82
Tristan Emmerson <tristan@stickerslug.com>
2020-08-03 10:54:58 -0700

    create designtimedbcontextfactory.cs.
    Co-authored-by: Sudolphus <micheal.hansen@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

---
+++ UniversityRegistrar/Models/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace UniversityRegistrar.Models
+{
+  public class UniversityRegistrarContextFactory : IDesignTimeDbContextFactory<UniversityRegistrarContext>
+  {
+    UniversityRegistrarContext IDesignTimeDbContextFactory<UniversityRegistrarContext>.CreateDbContext(string[] args)
+    {
+      IConfigurationRoot Configuration = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json")
+        .Build();
+
+      var builder = new DbContextOptionsBuilder<UniversityRegistrarContext>();
+      var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+      builder.UseMySql(connectionString);
+
+      return new UniversityRegistrarContext(builder.Options);
+    }
+  }
+}
