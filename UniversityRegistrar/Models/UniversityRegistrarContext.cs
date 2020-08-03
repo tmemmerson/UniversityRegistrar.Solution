9efc00a2f010ac535e3421668d1287a3ec174b57
Tristan Emmerson <tristan@stickerslug.com>
2020-08-03 15:38:45 -0700

    update context file to include dbset for department, studentdepartment, and coursedepartment.
    Co-authored-by: Sudolphus <micheal.hansen@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

--- UniversityRegistrar/Models/UniversityRegistrarContext.cs
+++ UniversityRegistrar/Models/UniversityRegistrarContext.cs
@@ -7,7 +7,9 @@ namespace UniversityRegistrar.Models
     public virtual DbSet<Course> Courses { get; set; }
     public virtual DbSet<Student> Students { get; set; }
     public virtual DbSet<StudentCourse> StudentCourse { get; set; }
-
+    public virtual DbSet<Department> Department { get; set; }
+    public virtual DbSet<StudentDepartment> StudentDepartment { get; set; }
+    public virtual DbSet<CourseDepartment> CourseDepartment { get; set; }
     public UniversityRegistrarContext(DbContextOptions options) : base(options) {}
   }
 }
