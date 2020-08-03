a051875b4aa726808fd104ee43bfbc3d143f98f9
Tristan Emmerson <tristan@stickerslug.com>
2020-08-03 13:09:53 -0700

    add index view to studentscontroller.cs.
    Co-authored-by: Sudolphus <micheal.hansen@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

---
+++ UniversityRegistrar/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace UniversityRegistrar.Controllers
+{
+  public class StudentsController : Controller
+  {
+    private readonly UniversityRegistrar _db;
+
+    public StudentsController(UniversityRegistrarContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      return View(_db.Items.ToList());
+    }
+  }
+}
