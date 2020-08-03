6b956f9d8035a6e91378ab564a09cceab460b23a
Tristan Emmerson <tristan@stickerslug.com>
2020-08-03 10:54:31 -0700

    create studentcourse.cs.
    Co-authored-by: Sudolphus <micheal.hansen@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

---
+++ UniversityRegistrar/Models/StudentCourse.cs
@@ -0,0 +1,11 @@
+namespace UniversityRegistrar.Models
+{
+  public class StudentCourse
+  {
+    public int StudentCourseId { get; set; }
+    public int StudentId { get; set; }
+    public int CourseId { get; set; }
+    public Student Student { get; set; }
+    public Course Course { get; set; }
+  }
+}
