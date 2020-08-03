e932e1d9ee74cb6bec86568d1dee2bc1eb2ba63d
Tristan Emmerson <tristan@stickerslug.com>
2020-08-03 11:18:51 -0700

    add index, create and details to coursescontroller.cs.
    Co-authored-by: Sudolphus <micheal.hansen@gmail.com>
    Co-authored-by: Tristan Emmerson <tristan@stickerslug.com>

---
+++ UniversityRegistrar/Controllers/CoursesController.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Mvc;
+using UniversityRegistrar.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace UniversityRegistrar.Controllers
+{
+  public class CoursesController : Controller
+  {
+    private readonly UniversityRegistrarContext _db;
+    public CoursesController(UniversityRegistrarContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Course> model = _db.Courses.ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Course course)
+    {
+      _db.Courses.Add(course);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = course.CourseId });
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisCourse = _db.Courses
+        .Include(course => course.Students)
+        .ThenInclude(join => join.Student)
+        .FirstOrDefault(course => course.CourseId == id);
+      return View(thisCourse);
+    } 
+
+
+
+  }
+
+}
